9c3ea121ba3396bd7529bcc81b16919f09b9607b
Tales313 <tales.11.98@gmail.com>
2021-12-22 20:51:31 -0300

    # 0.1.0-SNAPSHOT # > Comentario na SuiteCore > Add arquivo iml no gitignore

--- src/test/java/teste/SuiteCore.java
+++ src/test/java/teste/SuiteCore.java
@@ -12,3 +12,6 @@ import org.junit.runner.RunWith;
 })
 public class SuiteCore {
 }
+
+// Como mostrado no curso, essa classe vai dar erro se for executada com versões acima da 2019.1 do Intellij.
+// Eu tive que baixar e usar o Intellij 2019.1 para essa classe ser executada sem erro.
